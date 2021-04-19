f27f9b32a0027affae58edee1cce7b8a8e27dc6c
Maciej Burzynski <mjby@gft.com>
2021-04-19 21:48:43 +0200

    Extract to variables

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIAQ5XCDFX6FYQA5MOP"
+  secret_key = "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"
+  region     = "eu-central-1"
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "ec2_type" {
+    type = string
+	default = "t2.micro"
+}
